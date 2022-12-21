ab88ad58099a1046e2030af1d94845a8280a2684
FelixKleineBoesing <felix.boesing@t-online.de>
2022-12-21 17:42:42 +0100

    fix path

--- python/aoc/main.py
+++ python/aoc/main.py
@@ -5,7 +5,7 @@ from python.aoc.day_two import day_two
 from python.aoc.day_three import day_three
 
 def main():
-    file_path_general = "../../data/day_{}.txt"
+    file_path_general = "../../data/day_{}"
     print("Day 1")
     day_one(Path(file_path_general.format(1)))
     print("Day 2")
