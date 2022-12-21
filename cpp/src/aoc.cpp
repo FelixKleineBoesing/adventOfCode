77703814ba928dbe644133bf9056725c15f71eca
FelixKleineBoesing <felix.boesing@t-online.de>
2022-12-21 17:43:01 +0100

    update cpp

--- cpp/src/aoc.cpp
+++ cpp/src/aoc.cpp
@@ -6,18 +6,33 @@
 void AdventOfCode::day_one(const std::string& file_path)
 {
     std::ifstream dataFile;
-    // check if file exists
-
-
     dataFile.open(file_path, std::ios::in);
     if (!dataFile) {
         std::cout << "file does not exist" << std::endl;
     }
+    int32_t max_calories = 0;
+    int16_t max_calorie_elf = 1;
+    int32_t current_calories = 0;
+    int16_t current_elf = 1;
+
+
     if (dataFile.is_open()) {
         std::string line;
         while (std::getline(dataFile, line)) {
-            std::cout << line << std::endl;
+            if (line.empty()) {
+                if (current_calories > max_calories) {
+                    max_calories = current_calories;
+                    max_calorie_elf = current_elf;
+                }
+                current_calories = 0;
+                current_elf++;
+            } else {
+                current_calories += std::stoi(line);
+            }
         }
     }
     dataFile.close();
+
+    std::cout << "Elf " << max_calorie_elf << " has the most calories:" << max_calories << std::endl;
 }
+
