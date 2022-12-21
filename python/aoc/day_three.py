9538f4f4930d78ca03e941c0c425292966efcb90
FelixKleineBoesing <felix.boesing@t-online.de>
2022-12-21 16:35:44 +0100

    implement day three in python

--- python/aoc/day_three.py
+++ python/aoc/day_three.py
@@ -5,23 +5,57 @@ from typing import List
 from python.aoc.shared import read_file
 
 PRIORITY_MAPPING = {c: i + 1 for i, c in enumerate(string.ascii_lowercase)}
-PRIORITY_MAPPING.update({k: v + 26 for k, v in PRIORITY_MAPPING})
+PRIORITY_MAPPING.update({k.upper(): v + 26 for k, v in PRIORITY_MAPPING.items()})
 
 
 def day_three(file_path: Path):
+    # First part
+    priority_sums = first_part(file_path)
+    print("Priority sums: {}".format(priority_sums))
+
+    # Second part
+    prio_sum_second = second_part(file_path)
+    print("Priority sums second part: {}".format(prio_sum_second))
+    return priority_sums
+
+
+def second_part(file_path: Path):
+    sum_prio = 0
     lines = read_file(file_path)
+    for i in range(0, len(lines), 3):
+        group = [set(l.replace("\n", "")) for l in lines[i:(i+3)]]
+        inter = group[0] & group[1] & group[2]
+        sum_prio += PRIORITY_MAPPING[inter.pop()]
+
+    return sum_prio
+
+
+def first_part(file_path: Path):
+    lines = read_file(file_path)
+    priority_sums = get_priority_sum(lines)
+    return priority_sums
 
 
 def get_priority_sum(lines: List[str]):
     sum_prio = 0
     for line in lines:
-        mid = len(line)/2
-        first = set(list(line[:mid]))
-        second = set(list(line[mid:]))
-        diff = first.intersection(second)
-        sum_prio += get_prio_values(diff)
+        line = line.replace("\n", "")
+        mid = len(line)//2
+        first = line[:mid]
+        second = line[mid:]
+        duplicate = find_duplicate(first, second)
+        if duplicate:
+            sum_prio += PRIORITY_MAPPING[duplicate]
     return sum_prio
 
-def get_prio_values(diff: set):
-    return [PRIORITY_MAPPING[char] for char in diff]
 
+def find_duplicate(*args):
+    first, others = args[0], args[1:]
+    for f in first:
+        for arg in others:
+            if f not in arg:
+                break
+            return f
+
+if __name__ == "__main__":
+    pass
--- python/aoc/main.py
+++ python/aoc/main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from python.aoc.day_one import day_one
 from python.aoc.day_two import day_two
-
+from python.aoc.day_three import day_three
 
 def main():
     file_path_general = "../../data/day_{}.txt"
@@ -11,6 +11,8 @@ def main():
     print("Day 2")
     day_two(Path(file_path_general.format(2)))
 
+    print("Day 3")
+    day_three(Path(file_path_general.format(3)))
 
 
 if __name__ == "__main__":
