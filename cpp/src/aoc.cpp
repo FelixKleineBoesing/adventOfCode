f64421e8b17d4c5375c0be6f29d8d6c8ba68384d
FelixKleineBoesing <felix.boesing@t-online.de>
2022-12-21 16:36:04 +0100

    add file reading in cpp

--- cpp/src/aoc.cpp
+++ cpp/src/aoc.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>
 #include "static/aoc.h"
 
 void AdventOfCode::day_one(const std::string& file_path)
 {
-    std::fstream file;
-    file.open(file_path, std::ios::in);
-    std::cout << "Test!" << std::endl;
+    std::ifstream dataFile;
+    // check if file exists
+
+
+    dataFile.open(file_path, std::ios::in);
+    if (!dataFile) {
+        std::cout << "file does not exist" << std::endl;
+    }
+    if (dataFile.is_open()) {
+        std::string line;
+        while (std::getline(dataFile, line)) {
+            std::cout << line << std::endl;
+        }
+    }
+    dataFile.close();
 }
--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,10 +1,15 @@
-
+#include <map>
 #include <iostream>
 #include "static/aoc.h"
 
-int main() {
 
+int main() {
+    std::string file_path = "../../data/day_";
+    std::map<int, std::string> file_paths;
+    for (int i = 1; i <= 26; i++) {
+        file_paths[i] = file_path + std::to_string(i);
+    }
     AdventOfCode aoc;
-    aoc.day_one();
+    aoc.day_one(file_paths[1]);
     return 0;
 }
