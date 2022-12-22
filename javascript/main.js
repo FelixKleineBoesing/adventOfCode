986beda8f517887f1ea11befa2f9474b24725470
FelixKleineBoesing <felix.boesing@t-online.de>
2022-12-22 20:15:09 +0100

    javascript structure

--- javascript/main.js
+++ javascript/main.js
@@ -1,35 +1,39 @@
+const fs = require("fs");
+
+const base_path = "../data/day_"
+const file_paths = new Map();
+
+for (let i = 1; i < 26; i ++) {
+  file_paths.set(i, base_path + i)
+}
+
 'use strict';
-
-function counter() {
-  let seconds = 0;
-  setInterval(() => {
-    seconds += 1;
-    document.getElementById('dayOne').innerHTML = `<p>You have been here for ${seconds} seconds.</p>`;
-  }, 1000);
-}
-
-counter();
-
-
 function dayOne() {
-  
-}
-
-
-function readFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                alert(allText);
-            }
-        }
+  arr = syncReadFile(file_paths.get(1));
+  console.log(arr);
+  max_calories = 0;
+  max_calories_elf = 1;
+  current_calories = 0;
+  current_elf = 1;
+  for (let i = 0; i < arr.length(); i++) {
+    if (arr[i] == "") {
+      if (current_calories > max_calories) {
+        max_calories = current_calories;
+        max_calories_elf = current_elf;
+      }
+      current_elf ++;
+      current_calories = 0;
+    } else {
+      current_calories += Number(arr[i])
     }
-    rawFile.send(null);
+  }
+  document.getElementById('dayOne').innerHTML = "The max calories are: " + max_calories + ", carried by the elf: " + max_calories_elf;
+}
+dayOne()
+
+function syncReadFile(file_path)
+{
+    const contents = fs.readFileSync(file_path)
+    const arr = contents.split(/\r?\n/);
+    return arr
 }
