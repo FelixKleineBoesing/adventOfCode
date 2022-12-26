5f4ebaf4b5a1a535261a3ba69f1d925186335201
FelixKleineBoesing <felix.boesing@t-online.de>
2022-12-26 21:32:08 +0100

    day 9

--- python/aoc/day_nine.py
+++ python/aoc/day_nine.py
@@ -1,3 +1,4 @@
+import math
 from pathlib import Path
 
 from shared import read_file_clean
@@ -25,6 +26,23 @@ def day_nine_part_one(file_path: Path):
 
 def day_nine_part_two(file_path: Path):
     pass
+
+
+def update_tail(position_tail, pos_head):
+    x_diff, y_diff = position_tail[0] - pos_head[0], position_tail[1] - pos_head[1]
+    distance = math.sqrt(x_diff ** 2 + y_diff ** 2)
+    distance_diagonal = math.sqrt(2)
+    # distance when head is moving straight vertical/horzitontally
+    if distance == 2:
+        if x_diff == 0:
+            pass
+        new_tail_pos = [position_tail[0] + x_diff, position_tail[1] + y_diff]
+    elif distance + 0.001 > distance_diagonal:
+        pass
+    if abs(x_diff) > 0:
+        position_tail[0] += math.copysign(1, x_diff)
+
+
 
 
 def parse_direction(line: str) -> (int, int, int):
