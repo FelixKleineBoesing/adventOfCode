b95c99a75ce1ed47d6ce2a5624eebff448a6c0ea
FelixKleineBoesing <felix.boesing@t-online.de>
2023-01-02 23:48:14 +0100

    add day 16

---
+++ python/aoc/day_sixteen.py
@@ -0,0 +1,15 @@
+
+
+def day_sixteen(file_path):
+    print("Part 1")
+    day_sixteen_part_one(file_path)
+    print("Part 2")
+    day_sixteen_part_two(file_path)
+
+
+def day_sixteen_part_one(file_path):
+    pass
+
+
+def day_sixteen_part_two(file_path):
+    pass
